e9a9f527af83b83164e9f1d0366cf791e060b3a1
Artiana <hei.rojo.3@gmail.com>
2024-06-07 14:50:21 +0300

    test:ajout des tests

--- src/test/java/calculusratiocinator/AffirmationTest.java
+++ src/test/java/calculusratiocinator/AffirmationTest.java
@@ -4,4 +4,42 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AffirmationTest {
+
+    @Test
+    public void testLouEstPauvreEtLouEstGenereux() {
+        Affirmation mensonge1 = new Mensonge("Lou est pauvre");
+        Affirmation affirmation1 = new Affirmation("Lou est généreux");
+
+        Affirmation a1 = new AffirmationEt(mensonge1, affirmation1);
+        assertEquals("faux", a1.evaluer(), "Lou est pauvre et Lou est généreux");
+    }
+
+    @Test
+    public void testLouEstBeauDoncLouEstPauvre() {
+        Affirmation verite1 = new Verite("Lou est beau");
+        Affirmation mensonge1 = new Mensonge("Lou est pauvre");
+
+        Affirmation a2 = new AffirmationDonc(verite1, mensonge1);
+        assertEquals("faux", a2.evaluer(), "Lou est beau. Donc Lou est pauvre");
+    }
+
+    @Test
+    public void testLouEstPauvreDoncLouEstGenereux() {
+        Affirmation mensonge1 = new Mensonge("Lou est pauvre");
+        Affirmation affirmation1 = new Affirmation("Lou est généreux");
+
+        Affirmation a3 = new AffirmationDonc(mensonge1, affirmation1);
+        assertEquals("vrai", a3.evaluer(), "Lou est pauvre. Donc Lou est généreux");
+    }
+
+    @Test
+    public void testLouEstBeauOuLouEstGenereuxDoncLouEstPauvre() {
+        Affirmation verite1 = new Verite("Lou est beau");
+        Affirmation affirmation1 = new Affirmation("Lou est généreux");
+        Affirmation mensonge1 = new Mensonge("Lou est pauvre");
+
+        Affirmation a4 = new AffirmationDonc(new AffirmationOu(verite1, affirmation1), mensonge1);
+        assertEquals("faux", a4.evaluer(), "Lou est beau ou Lou est généreux. Donc Lou est pauvre");
+    }
+
 }
